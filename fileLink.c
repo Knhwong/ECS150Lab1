049276d16423324cc0ee26dd4c75f88e252d66c9
Knhwong <47486931+Knhwong@users.noreply.github.com>
2022-04-19 16:22:50 -0700

    Update fileLink.c

--- fileLink.c
+++ fileLink.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
 
 //With reference from https://linuxhint.com/stat-system-call-linux/
 //To understand how to use the function stat and lstat from lecture.
@@ -14,29 +16,59 @@ int main(int argc, char * argv[]) {
    char * outMessage;
 
    struct stat file1;
+   struct stat file1SymCheck;
    struct stat file2SymCheck;
-   struct stat file2HardCheck;
+   struct stat file2;
 
    //Arg 1 is being passed to S2 as the symboliic check.
-   int s1 = stat(argv[2], &file1);
-   int s2S = lstat(argv[1], &file2SymCheck);
-   int s2H = stat(argv[1], &file2HardCheck);
+   int s1 = stat(argv[1], &file1);
+   int s1S = lstat(argv[1], &file1SymCheck);
+   int s2S = lstat(argv[2], &file2SymCheck);
+   int s2H = stat(argv[2], &file2);
 
    if (s1 == 0  && s2S == 0 && s2H == 0) {
-      if (file1.st_ino == file2SymCheck.st_ino) {
-         outMessage = "These files are linked";
-      } 
-      else {
-         if (file1.st_ino == file2HardCheck.st_ino) {
-            outMessage = "file1 is a symbolic link to file2";
+      printf("%lu\n", file1.st_ino);
+      printf("%lu\n", file1SymCheck.st_ino);
+      printf("%lu\n", file2.st_ino);
+      printf("%lu\n", file2SymCheck.st_ino);
+      printf("%lu\n", file2SymCheck.st_nlink);
+
+      
+      if (file1.st_ino == file2.st_ino){
+         if (file1.st_ino == file1SymCheck.st_ino && file2.st_ino == file2SymCheck.st_ino) {
+            printf("Files are linked \n");
+         } else if (file1.st_ino != file1SymCheck.st_ino && file2.st_ino == file2SymCheck.st_ino){
+            //Check for indirection
+            char * buffer[file1SymCheck.st_size +1];
+            readlink(argv[1], buffer, file1SymCheck.st_size +1);
+            //Can't compare strings due to hard link vs symlink
+            struct stat buff;
+            lstat(buffer, &buff);
+            if (file1.st_ino == buff.st_ino) {
+               printf("%s is a symlink to %s \n", argv[1],argv[2]);
+            } else {
+               printf("Files are linked \n");
+            }
+
+         } else if (file1.st_ino == file1SymCheck.st_ino && file2.st_ino != file2SymCheck.st_ino) {
+            char * buffer[file2SymCheck.st_size +1];
+            readlink(argv[2], buffer, file2SymCheck.st_size + 1);
+            struct stat buff;
+            lstat(buffer, &buff);
+            if (file2.st_ino == buff.st_ino) {
+               printf("%s is a symlink to %s \n", argv[2],argv[1]);
+            } else {
+               printf("Files are linked \n");
+            }
+         } else {
+            printf("Files are linked \n");
          }
-         else {
-            outMessage = "The files are not linked";
-         }
+      } else {
+         printf("Files are not linked \n");
       }
-      printf("%s\n", outMessage);
+      
    } else {
-      perror("File check failed!");
+      perror("perror");
    }
 
 
